b3cb5e4a9c3076adbaff841040d3202efbc0cc11
Joey <smallstars10@gmail.com>
2025-07-05 16:46:09 +0800

    feat: add clerk and middleware

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -4,15 +4,20 @@ import type { PropsWithChildren } from 'react'
 
 import ThemeProvider from './ThemeProvider'
 
+import { Toaster } from '@/components/ui/Sonner'
+
 const Providers = ({ children }: PropsWithChildren) => (
-  <ThemeProvider
-    attribute="class"
-    defaultTheme="system"
-    disableTransitionOnChange
-    enableSystem
-  >
-    {children}
-  </ThemeProvider>
+  <>
+    <Toaster />
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      disableTransitionOnChange
+      enableSystem
+    >
+      {children}
+    </ThemeProvider>
+  </>
 )
 
 export default Providers
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -20,14 +21,16 @@ const RootLayout = ({
 }: Readonly<{
   children: ReactNode
 }>) => (
-  <html lang="en" suppressHydrationWarning>
-    <body className={inter.className}>
-      <Providers>
-        <Navbar />
-        <Container className="py-20">{children}</Container>
-      </Providers>
-    </body>
-  </html>
+  <ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Providers>
+          <Navbar />
+          <Container className="py-20">{children}</Container>
+        </Providers>
+      </body>
+    </html>
+  </ClerkProvider>
 )
 
 export default RootLayout

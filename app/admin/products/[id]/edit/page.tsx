65bb75f3fb6d98a7b05f7c424b60981f7dff1bb7
Joey <smallstars10@gmail.com>
2025-07-09 22:21:46 +0800

    feat: add update image function

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,3 +1,75 @@
-const EditProductPage = () => <div>EditProductPage </div>
+import { SubmitButton } from '@/components/form/Buttons'
+import CheckboxInput from '@/components/form/CheckBoxInput'
+import FormContainer from '@/components/form/FormContainer'
+import FormInput from '@/components/form/FormInput'
+import ImageInputContainer from '@/components/form/ImageInputContainer'
+import PriceInput from '@/components/form/PriceInput'
+import TextAreaInput from '@/components/form/TextAreaInput'
+import {
+  fetchAdminProductDetails,
+  updateProduct,
+  updateProductImage
+} from '@/utils/dbActions'
+
+interface EditProductPageProps {
+  params: {
+    id: string
+  }
+}
+
+const EditProductPage = async ({ params: { id } }: EditProductPageProps) => {
+  const product = await fetchAdminProductDetails(id)
+
+  const { name, company, description, featured, price, image } = product
+
+  return (
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">update product</h1>
+      <div className="border p-8 rounded-md">
+        <ImageInputContainer
+          action={updateProductImage}
+          image={image}
+          name={name}
+          text="update image"
+        >
+          <input name="id" type="hidden" value={id} />
+          <input name="url" type="hidden" value={image} />
+        </ImageInputContainer>
+        <FormContainer action={updateProduct}>
+          <div className="grid gap-4 md:grid-cols-2 my-4">
+            <input name="id" type="hidden" value={id} />
+            <FormInput
+              defaultValue={name}
+              label="product name"
+              name="name"
+              type="text"
+            />
+            <FormInput
+              defaultValue={company}
+              label="company"
+              name="company"
+              type="text"
+            />
+
+            <PriceInput defaultValue={price} />
+          </div>
+          <TextAreaInput
+            defaultValue={description}
+            labelText="product description"
+            name="description"
+          />
+          <div className="mt-6">
+            <CheckboxInput
+              defaultChecked={featured}
+              label="featured"
+              name="featured"
+            />
+          </div>
+          <SubmitButton className="mt-8" text="update product" />
+        </FormContainer>
+      </div>
+    </section>
+  )
+}
 
 export default EditProductPage
---
+++ components/form/ImageInputContainer.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { PropsWithChildren, useState } from 'react'
+import Image from 'next/image'
+
+import { Button } from '../ui/Button'
+import { SubmitButton } from './Buttons'
+import FormContainer from './FormContainer'
+import ImageInput from './ImageInput'
+
+import type { ActionFunction } from '@/utils/types'
+
+interface ImageInputContainerProps {
+  image: string
+  name: string
+  action: ActionFunction
+  text: string
+}
+
+const ImageInputContainer = ({
+  image,
+  name,
+  action,
+  text,
+  children
+}: PropsWithChildren<ImageInputContainerProps>) => {
+  const [isUpdateFormVisible, setUpdateFormVisible] = useState(false)
+
+  return (
+    <div className="mb-8">
+      <Image
+        alt={name}
+        className="rounded-md object-cover mb-4 w-[200px] h-[200px]"
+        height={200}
+        src={image}
+        width={200}
+      />
+
+      <Button
+        size="sm"
+        variant="outline"
+        onClick={() => setUpdateFormVisible((prev) => !prev)}
+      >
+        {text}
+      </Button>
+      {isUpdateFormVisible && (
+        <div className="max-w-md mt-4">
+          <FormContainer action={action}>
+            {children}
+            <ImageInput />
+            <SubmitButton size="sm" />
+          </FormContainer>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ImageInputContainer

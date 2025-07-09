2d1c43c8ad8a687577ee3c403addd89fe6dc12aa
Joey <smallstars10@gmail.com>
2025-07-09 14:58:11 +0800

    feat: implement admin products page

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,3 +1,57 @@
-const ProductsPage = () => <div>Admin Products Page</div>
+import Link from 'next/link'
 
-export default ProductsPage
+import EmptyList from '@/components/global/EmptyList'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/Table'
+import { fetchAdminProducts } from '@/utils/dbActions'
+import { formatCurrency } from '@/utils/formatCurrency'
+
+const ItemsPage = async () => {
+  const productList = await fetchAdminProducts()
+
+  if (productList.length === 0) return <EmptyList />
+
+  return (
+    <section>
+      <Table>
+        <TableCaption className="capitalize">
+          Total products: {productList.length}
+        </TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Product Name</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {productList.map(({ id, name, company, price }) => (
+            <TableRow key={id}>
+              <TableCell>
+                <Link
+                  className="underline text-muted-foreground tracking-wide capitalize"
+                  href={`/products/${id}`}
+                >
+                  {name}
+                </Link>
+              </TableCell>
+              <TableCell>{company}</TableCell>
+              <TableCell>{formatCurrency(price)}</TableCell>
+              <TableCell className="flex items-center gap-x-2"></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </section>
+  )
+}
+
+export default ItemsPage
